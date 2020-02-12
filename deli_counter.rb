a108802d9d1d58d84cba22c83a893b2330e8e567
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 19:10:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+
+    deli.each_with_index do |customer, index|
+      string << " #{index + 1}. #{customer}"
   end
 end
